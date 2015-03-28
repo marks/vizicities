64f61e4e2e1867a0ecc6b080a6f153379d0a007a
Mark Silverberg <mark.silverberg@socrata.com>
2015-03-28 18:13:17 -0400

    created BlueprintOutputPoints.js to tinker with mapping points

---
+++ examples/geojson-points/main.js
@@ -0,0 +1,221 @@
+var world = new VIZI.World({
+  viewport: document.querySelector("#vizicities-viewport"),
+  layersUI: true,
+  picking: true,
+  center: new VIZI.LatLon(39, -77) // Silver Spring, MD
+});
+
+var controls = new VIZI.ControlsMap(world.camera, {
+  viewport: world.options.viewport
+});
+
+var pickControls = new VIZI.ControlsMousePick(world.camera, {
+  scene: world.scene
+});
+
+var descriptionUI = new VIZI.DescriptionUI({
+  title: "Basic example",
+  body: "This is a basic example showing a 2D basemap, 3D building tiles and a choropleth of population density."
+});
+
+var mapConfig = {
+  input: {
+    type: "BlueprintInputMapTiles",
+    options: {
+      tilePath: "https://a.tiles.mapbox.com/v3/examples.map-i86l3621/{z}/{x}/{y}@2x.png"
+    }
+  },
+  output: {
+    type: "BlueprintOutputImageTiles",
+    options: {
+      grids: [{
+        zoom: 19,
+        tilesPerDirection: 3,
+        cullZoom: 17
+      }, {
+        zoom: 18,
+        tilesPerDirection: 3,
+        cullZoom: 16
+      }, {
+        zoom: 17,
+        tilesPerDirection: 3,
+        cullZoom: 15
+      }, {
+        zoom: 16,
+        tilesPerDirection: 3,
+        cullZoom: 14
+      }, {
+        zoom: 15,
+        tilesPerDirection: 3,
+        cullZoom: 13
+      }, {
+        zoom: 14,
+        tilesPerDirection: 3,
+        cullZoom: 12
+      }, {
+        zoom: 13,
+        tilesPerDirection: 5,
+        cullZoom: 11
+      }]
+    }
+  },
+  triggers: [{
+    triggerObject: "output",
+    triggerName: "initialised",
+    triggerArguments: ["tiles"],
+    actionObject: "input",
+    actionName: "requestTiles",
+    actionArguments: ["tiles"],
+    actionOutput: {
+      tiles: "tiles" // actionArg: triggerArg
+    }
+  }, {
+    triggerObject: "output",
+    triggerName: "gridUpdated",
+    triggerArguments: ["tiles"],
+    actionObject: "input",
+    actionName: "requestTiles",
+    actionArguments: ["tiles"],
+    actionOutput: {
+      tiles: "tiles" // actionArg: triggerArg
+    }
+  }, {
+    triggerObject: "input",
+    triggerName: "tileReceived",
+    triggerArguments: ["image", "tile"],
+    actionObject: "output",
+    actionName: "outputImageTile",
+    actionArguments: ["image", "tile"],
+    actionOutput: {
+      image: "image", // actionArg: triggerArg
+      tile: "tile"
+    }
+  }]
+};
+
+var switchboardMap = new VIZI.BlueprintSwitchboard(mapConfig);
+switchboardMap.addToWorld(world);
+
+var buildingsConfig = {
+  input: {
+    type: "BlueprintInputGeoJSON",
+    options: {
+      tilePath: "http://vector.mapzen.com/osm/buildings/{z}/{x}/{y}.json"
+    }
+  },
+  output: {
+    type: "BlueprintOutputBuildingTiles",
+    options: {
+      grids: [{
+        zoom: 15,
+        tilesPerDirection: 1,
+        cullZoom: 13
+      }],
+      workerURL: "../../build/vizi-worker.min.js"
+    }
+  },
+  triggers: [{
+    triggerObject: "output",
+    triggerName: "initialised",
+    triggerArguments: ["tiles"],
+    actionObject: "input",
+    actionName: "requestTiles",
+    actionArguments: ["tiles"],
+    actionOutput: {
+      tiles: "tiles" // actionArg: triggerArg
+    }
+  }, {
+    triggerObject: "output",
+    triggerName: "gridUpdated",
+    triggerArguments: ["tiles", "newTiles"],
+    actionObject: "input",
+    actionName: "requestTiles",
+    actionArguments: ["tiles"],
+    actionOutput: {
+      tiles: "newTiles" // actionArg: triggerArg
+    }
+  }, {
+    triggerObject: "input",
+    triggerName: "tileReceived",
+    triggerArguments: ["geoJSON", "tile"],
+    actionObject: "output",
+    actionName: "outputBuildingTile",
+    actionArguments: ["buildings", "tile"],
+    actionOutput: {
+      buildings: {
+        process: "map",
+        itemsObject: "geoJSON",
+        itemsProperties: "features",
+        transformation: {
+          outline: "geometry.coordinates",
+          height: "properties.height",
+          minHeight: "properties.min_height"
+        }
+      },
+      tile: "tile"
+    }
+  }]
+};
+
+var switchboardBuildings = new VIZI.BlueprintSwitchboard(buildingsConfig);
+switchboardBuildings.addToWorld(world);
+
+var pointsConfig = {
+  input: {
+    type: "BlueprintInputGeoJSON",
+    options: {
+      path: "./data/sample.geojson"
+    }
+  },
+  output: {
+    type: "BlueprintOutputPoints",
+    options: {
+      name: "Restauraunt Inspections",
+      defaultColor: 0x00ff00
+    }
+  },
+  triggers: [{
+    triggerObject: "output",
+    triggerName: "initialised",
+    triggerArguments: [],
+    actionObject: "input",
+    actionName: "requestData",
+    actionArguments: [],
+    actionOutput: {}
+  }, {
+    triggerObject: "input",
+    triggerName: "dataReceived",
+    triggerArguments: ["geoJSON"],
+    actionObject: "output",
+    actionName: "outputPoints",
+    actionArguments: ["data"],
+    actionOutput: {
+      data: {
+        // Loop through each item in trigger.geoJSON and return a new array of processed values (a map)
+        process: "map",
+        itemsObject: "geoJSON",
+        itemsProperties: "features",
+        // Return a new object for each item with the given properties
+        transformation: {
+          coordinates: "geometry.coordinates"
+        }
+      }
+    }
+  }]
+};
+
+var switchboardPoints = new VIZI.BlueprintSwitchboard(pointsConfig);
+switchboardPoints.addToWorld(world);
+
+var clock = new VIZI.Clock();
+
+var update = function() {
+  var delta = clock.getDelta();
+
+  world.onTick(delta);
+  world.render();
+
+  window.requestAnimationFrame(update);
+};
+
+update();
---
+++ src/Blueprint/BlueprintOutputPoints.js
@@ -0,0 +1,124 @@
+/* globals window, _, VIZI, THREE */
+(function() {
+  "use strict";
+
+/**
+ * Blueprint debug points output
+ * @author Robin Hawkes - vizicities.com
+ * @author Mark Silverberg - mark.silverberg@socrata.com
+ */  
+
+  // output: {
+  //   type: "BlueprintOutputPoints",
+  //   options: {name: 'My layer', defaultColor: 0xff0000}
+  // }
+  VIZI.BlueprintOutputPoints = function(options) {
+    var self = this;
+
+    VIZI.BlueprintOutput.call(self, options);
+
+    _.defaults(self.options, {
+      name: "Points",
+      defaultColor: 0xff0000
+    });
+
+    // Triggers and actions reference
+    self.triggers = [
+      {name: "initialised", arguments: []}
+    ];
+
+    self.actions = [
+      {name: "outputPoints", arguments: ["data"]}
+    ];
+
+    self.name = self.options.name;
+
+    self.world;
+  };
+
+  VIZI.BlueprintOutputPoints.prototype = Object.create( VIZI.BlueprintOutput.prototype );
+
+  // Initialise instance and start automated processes
+  VIZI.BlueprintOutputPoints.prototype.init = function() {
+    var self = this;
+
+    self.emit("initialised");
+  };
+
+  // {
+  //   coordinates: [lon, lat]
+  // }
+  VIZI.BlueprintOutputPoints.prototype.outputPoints = function(data) {
+    var self = this;
+
+    var material = new THREE.MeshBasicMaterial({
+      color: self.options.defaultColor,
+      // vertexColors: THREE.VertexColors,
+      // ambient: 0xffffff,
+      // emissive: 0xcccccc,
+      shading: THREE.FlatShading
+    });
+
+    var barGeom = new THREE.BoxGeometry( 40, 1, 40 );
+
+    // Shift each vertex by half the bar height
+    // This means it will scale from the bottom rather than the centre
+    var vertices = barGeom.vertices;
+    for (var v = 0; v < vertices.length; v++) {
+      vertices[v].y += 0.5;
+    }
+
+    var combinedGeom = new THREE.Geometry();
+
+    _.each(data, function(point) {
+      var coords = point.coordinates;
+
+      var offset = new VIZI.Point();
+
+      var geoCoord = self.world.project(new VIZI.LatLon(coords[1], coords[0]));
+
+      offset.x = -1 * geoCoord.x;
+      offset.y = -1 * geoCoord.y;
+
+      // TODO: Get this from options
+      var height = 1000;
+
+      var mesh = new THREE.Mesh(barGeom);
+
+      mesh.scale.y = height;
+
+      // Offset
+      mesh.position.x = -1 * offset.x;
+      mesh.position.z = -1 * offset.y;
+
+      // Flip as they are up-side down
+      // mesh.rotation.x = 90 * Math.PI / 180;
+
+      mesh.matrixAutoUpdate && mesh.updateMatrix();
+      combinedGeom.merge(mesh.geometry, mesh.matrix);
+    });
+
+    // Move merged geom to 0,0 and return offset
+    var offset = combinedGeom.center();
+
+    var combinedMesh = new THREE.Mesh(combinedGeom, material);
+
+    // Use previously calculated offset to return merged mesh to correct position
+    // This allows frustum culling to work correctly
+    combinedMesh.position.x = -1 * offset.x;
+
+    // Removed for scale center to be correct
+    // Offset with applyMatrix above
+    combinedMesh.position.y = -1 * offset.y;
+
+    combinedMesh.position.z = -1 * offset.z;
+
+    self.add(combinedMesh);
+  };
+
+  VIZI.BlueprintOutputPoints.prototype.onAdd = function(world) {
+    var self = this;
+    self.world = world;
+    self.init();
+  };
+}());
